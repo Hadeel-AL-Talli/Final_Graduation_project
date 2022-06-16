2ae68f8ca3bd11b89ea8c66600b2ddf99384e4cf
Nada aljarrah <nadajarrah627@gmail.com>
2022-06-16 13:01:03 +0300

    SubCategories_Products_UI

--- lib/modules/SubCategories/products_screen.dart
+++ lib/modules/SubCategories/products_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,8 +7,6 @@ import 'package:graduation_project/shared/components/product_widget.dart';
 import 'package:graduation_project/shared/network/local/shared_pref_controller.dart';
 
 import '../../models/product.dart';
-
-
 
 class ProudctScreen extends StatefulWidget {
   final int id;
@@ -25,61 +21,73 @@ class _ProudctScreenState extends State<ProudctScreen> {
   late Future<List<Product>> _future;
   List<Product> _products = <Product>[];
   void initState() {
-    
     super.initState();
-    
+
     _future = HomeApiController().getProducts(widget.id.toString());
   }
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        title:  Text(
-          "Products".tr,
-          style: Theme.of(context).textTheme.labelMedium?.copyWith(fontSize: 20)
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text("Products".tr,
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium
+                  ?.copyWith(fontSize: 20)),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios,),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+        body: FutureBuilder<List<Product>>(
+            future: _future,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                print('watit');
 
-    body:  FutureBuilder<List<Product>> (
-      future: _future,
-      builder: (context, snapshot){
-           if(snapshot.connectionState == ConnectionState.waiting){
-            print('watit');
-            
-        return Center(child: CircularProgressIndicator(),);
-           }
-           else if (snapshot.hasData && snapshot.data!.isNotEmpty){
-            
-             _products = snapshot.data ?? [];
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                _products = snapshot.data ?? [];
 
-            return GridView.builder(
-              itemCount: _products.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-             itemBuilder: (context, index){
-             // return Text(_products[index].nameEn,style:Theme.of(context).textTheme.labelMedium?.copyWith(fontSize: 20));
-            return ProductWidget(imageUrl: _products[index].imageUrl, name:SharedPrefController().language=='en'? _products[index].nameEn:_products[index].nameAr, price: _products[index].price);
-             });
-           }
-
-           else{
-             return Center(
-               child: Text('No Data', style:Theme.of(context).textTheme.labelMedium?.copyWith(fontSize: 20)),
-             );
-           }
-           
-      }
-    )
-    );
-  
+                return Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+                  child: GridView.builder(
+                      itemCount: _products.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1 / 1.45,
+                        // mainAxisSpacing: 10,
+                        // crossAxisSpacing: 10
+                      ),
+                      itemBuilder: (context, index) {
+                        // return Text(_products[index].nameEn,style:Theme.of(context).textTheme.labelMedium?.copyWith(fontSize: 20));
+                        return ProductWidget(
+                            imageUrl: _products[index].imageUrl,
+                            name: SharedPrefController().language == 'en'
+                                ? _products[index].nameEn
+                                : _products[index].nameAr,
+                            price: _products[index].price);
+                      }),
+                );
+              } else {
+                return Center(
+                  child: Text('No Data',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelMedium
+                          ?.copyWith(fontSize: 20)),
+                );
+              }
+            }));
   }
 }
